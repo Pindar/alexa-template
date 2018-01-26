b86d4d3879332ac0ded9731adcdc2a2f28e6087a
Simon Dittlmann <131621+Pindar@users.noreply.github.com>
2018-01-26 23:00:35 +0100

    chore: improve build time

--- lambda/custom/webpack.config.js
+++ lambda/custom/webpack.config.js
@@ -1,10 +1,15 @@
 const slsw = require('serverless-webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   stats: 'minimal',
   devtool: 'source-map',
+  plugins: [
+    new HardSourceWebpackPlugin()
+  ],
   module: {
     rules: [
       {
